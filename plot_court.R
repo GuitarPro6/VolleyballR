13b3accbc875053ca1bf4eb77bbf787dd596980f
Jon Bown <jonvzw6@gmail.com>
2016-11-07 20:21:54 -0700

    Update plot_court.R

--- plot_court.R
+++ plot_court.R
@@ -92,7 +92,7 @@ court_without_three = rbind(court_points , foul_circle_top, foul_circle_bottom, 
 #This method draws the court, contains the coordinates that put limits on what can be drawn
 court = ggplot() + geom_path(data = court_points,
             aes(x = x, y = y, group = desc, linetype = "solid"),
-            color = "#999999") + scale_linetype_manual(values = c("solid", "longdash"), guide = FALSE) + + geom_path(data = line_points,aes(x = x, y = y, group = "dottedLine", linetype = "solid")) + coord_fixed(ylim = c(0, 30), xlim = c(-27, 27)) +
+            color = "#999999") + scale_linetype_manual(values = c("solid", "longdash"), guide = FALSE) + geom_path(data = line_points,aes(x = x, y = y, group = "dottedLine", linetype = "solid")) + coord_fixed(ylim = c(0, 30), xlim = c(-27, 27)) +
   theme_court(base_size = 22) 
 
 

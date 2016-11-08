4890a4566a7758b2b30efd2033c87e145311d1fa
Jon Bown <jonvzw6@gmail.com>
2016-11-07 19:37:23 -0700

    Update plot_court.R

--- plot_court.R
+++ plot_court.R
@@ -106,7 +106,7 @@ court = ggplot() +
   scale_linetype_manual(values = c("solid", "longdash"), guide = FALSE) +
   coord_fixed(ylim = c(0, 30), xlim = c(-27, 27)) +
   theme_court(base_size = 22) + geom_path(data = line_points,
-            aes(x = x, y = y, group = "dottedLine", linetype = "dotdash"),
+            aes(x = x, y = y, group = "dottedLine", linetype = "dash"),
             color = "#999999")
 
 short_three_court = ggplot() +

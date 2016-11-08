8d7ef899e49893c0baf38ad849981bdbe9d0084e
Jon Bown <jonvzw6@gmail.com>
2016-11-07 19:35:12 -0700

    Update plot_court.R

--- plot_court.R
+++ plot_court.R
@@ -102,7 +102,7 @@ short_three_court_points = mutate(short_three_court_points , dash = (desc == "fo
 court = ggplot() +
   geom_path(data = court_points,
             aes(x = x, y = y, group = desc, linetype = dash),
-            color = "#999999") + +  geom_path(data = line_points,
+            color = "#999999") +  geom_path(data = line_points,
             aes(x = x, y = y, group = "dottedLine", linetype = "dotdash"),
             color = "#999999")
   scale_linetype_manual(values = c("solid", "longdash"), guide = FALSE) +
